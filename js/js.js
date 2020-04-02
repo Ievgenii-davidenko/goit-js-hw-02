d9f0eb13ea69e020a662ef427f98824a57179874
Ievgenii <bijibnui88@gmail.com>
2020-04-02 20:12:42 +0300

    added index.html

---
+++ js/js.js
@@ -0,0 +1,40 @@
+// const userChoice = prompt
+//     "Выберите символ ( 1 - kkk, 2 - nog, 3 - stone)";
+//     let score = 0;
+//     if (userChoice === null) {
+//         alter (":(");
+
+//     } else {
+//         computerChoise = Number.parseInt(Math.random() * (3-1) + 1);
+//         if (computerChoice === userChoice)
+//         switch (userChoice) {
+//             case "1":
+//                 if (computerChoice === 1)
+//                 breack;
+//                 case "2";
+//                 breack;
+//                 case "3";
+//                 breack;
+//         }
+//     }
+//     function comparison (userChoice, )
+
+// const answer = parseInt(Math.random() * 10);
+//     while (true) {
+//         var userAnswer = prompt('Введите число от 0 до 10. Для выхода нажмите q');
+//         if (userAnswer == 'q') {
+//             break;
+//         }
+//         userAnswer = parseInt(userAnswer);
+ 
+//         if (userAnswer > answer) {
+//             alert('Ваш ответ слишком большой');
+//         } else if (userAnswer < answer) {
+//             alert('Ваш ответ слишком маленький');
+//         } else if (userAnswer == answer) {
+//             alert('Вы угадали!');
+//             break;
+//         } else {
+//             alert('Необходимо ввести число!');
+//         }
+//     }
